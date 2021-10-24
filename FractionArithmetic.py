6f2f35e4c9468ed4d1721666d617cd7c48be51bb
jenchessica <jessica7chen@gmail.com>
2021-10-24 11:29:38 -0700

    Wow this was so fun doing fraction stuff

---
+++ FractionArithmetic.py
@@ -0,0 +1,21 @@
+import LCM
+
+def kat(a,b,c,d,e,f,captain):
+    m = a*c+b
+    n = d*f+e
+
+    if captain == "*":
+       p = m*n
+       q = c*f
+    elif captain == "+":
+       q = LCM.LCM(c,f)
+       p = m*q//c+n*q//f
+
+    lcm = LCM.GCD(p,q)
+    cat = p//lcm
+    dog = q//lcm
+    hamster = cat//dog
+    rat = cat%dog
+    print(str(hamster) + " " + str(rat) + "/" + str(dog))
+
+kat(1,2,3,4,5,6,"+")
