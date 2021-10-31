474b3631f777d18ace173dbf475d01c614ee9d3d
jenchessica <jessica7chen@gmail.com>
2021-10-31 11:08:45 -0700

    My fuzzy socks make me look like cookie monster

--- FractionArithmetic.py
+++ FractionArithmetic.py
@@ -3,6 +3,7 @@ import LCM
 def kat(a,b,c,d,e,f,captain):
     m = a*c+b
     n = d*f+e
+    isNegative = False
 
     if captain == "*":
        p = m*n
@@ -10,12 +11,25 @@ def kat(a,b,c,d,e,f,captain):
     elif captain == "+":
        q = LCM.LCM(c,f)
        p = m*q//c+n*q//f
+    elif captain == "/":
+       p = m*f
+       q = c*n
+    elif captain == "-":
+       q = LCM.LCM(c,f)
+       p = m*q//c-n*q//f
+       if p < 0:
+          isNegative = True
+          q = -q
 
     lcm = LCM.GCD(p,q)
     cat = p//lcm
     dog = q//lcm
     hamster = cat//dog
     rat = cat%dog
-    print(str(hamster) + " " + str(rat) + "/" + str(dog))
+    if isNegative:
+       print("-" + str(hamster) + " " + str(rat) + "/" + str(dog))
+    else:
+       print(str(hamster) + " " + str(rat) + "/" + str(dog))
 
-kat(1,2,3,4,5,6,"+")
+
+kat(1,2,3,4,5,6,"-")
