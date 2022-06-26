025f592874e57a8471331bfb287a503521b9f6b6
jenchessica <jessica7chen@gmail.com>
2022-06-26 11:07:40 -0700

    I don't have any pretty necklaces :(

--- Necklace.py
+++ Necklace.py
@@ -9,7 +9,7 @@ def drawbead(axes,radius,x,y, color):
     colored_circle = plt.Circle( (x, y), 0.5*radius, color=color)
     axes.add_artist(colored_circle)
 
-def drawnecklace(arr):
+def drawnecklace(arr,desc):
     #create axis
     figure, axes = plt.subplots()
     axes.set_aspect( 1 )
@@ -20,12 +20,30 @@ def drawnecklace(arr):
         drawbead(axes,r,x,0.3, arr[i])
 
     #show
-    plt.title( 'Circle' )
+    plt.title(desc)
     plt.show()
+
+def ispretty(arr):
+    stack = []
+    for i in range (0, len(arr)):
+        if len(stack)>0 and arr[i]==stack[-1]:
+            stack.pop()
+        else:
+            stack.append(arr[i])
+
+    if len(stack)==0:
+        return True
+    else:
+        return False
 
 #drawbead(0.1, 0.3, 0.3, "r")
 
-colors=["r", "b", "m", "y", "r", "b", "m", "y", "r", "b"]
-drawnecklace(colors)
+colors=["y", "r", "b", "m", "m", "b", "r", "y"]
 
+x=ispretty(colors)
+
+if x:
+    drawnecklace(colors,'pretty')
+else:
+    drawnecklace(colors,'ugly')
 
